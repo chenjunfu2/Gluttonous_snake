6d0da4da4280af790ed9c97d278e25bdc1c28468
chenjunfu2 <64954184+chenjunfu2@users.noreply.github.com>
2023-12-03 11:51:21 +0800

    更新了蛇的运动判断逻辑和失败判断逻辑
    现在蛇头的下一个位置是蛇尾时，游戏不再失败了

--- Game_Control.hpp
+++ Game_Control.hpp
@@ -16,7 +16,7 @@ class Game_Control
 
 		if (enMapType == Game_Data::Map_Type::Head ||
 			enMapType == Game_Data::Map_Type::Body ||
-			enMapType == Game_Data::Map_Type::Tail ||
+			//enMapType == Game_Data::Map_Type::Tail ||//允许碰到蛇尾，因为蛇在运动时蛇头紧跟着蛇尾时实际上是同时移动的，并不会吃掉
 			enMapType == Game_Data::Map_Type::Wall)//撞墙或吃掉自身，游戏结束
 		{
 			return true;
@@ -185,55 +185,73 @@ public:
 
 	static bool Move(Game_Data &csGameData)//移动出边界后环绕
 	{
-		//吃食物判断
-		bool bEatFood = false;
+		//保存当前蛇头方向
+		Game_Data::Move_Direct enCurrentHeadDirect = csGameData.GetHeadDirect();
+		//获取蛇头向当前方向移动一格的位置
+		My_Point stNewSnakeHead = csGameData.GetSnakeHead() + Game_Data::stSnakeMove[(long)enCurrentHeadDirect];
+		//越界环绕，环绕只需处理蛇头与蛇尾，蛇身部分无需改变
+		csGameData.Surround(stNewSnakeHead);
 
-		//更新蛇头
+		//判断是否输了
+		if (Lose(csGameData, stNewSnakeHead))
 		{
-			//保存当前蛇头方向
-			Game_Data::Move_Direct enCurrentHeadDirect = csGameData.GetHeadDirect();
-
-			//将蛇头向当前方向移动一格
-			My_Point stNewSnakeHead = csGameData.GetSnakeHead() + Game_Data::stSnakeMove[(long)enCurrentHeadDirect];
-			//越界环绕//环绕只需处理蛇头与蛇尾，蛇身部分无需改变
-			csGameData.Surround(stNewSnakeHead);
-
-			//判断是否输了
-			if (Lose(csGameData, stNewSnakeHead))
-			{
-				return false;
-			}
-
-			//递增移动距离
-			csGameData.IncTravelDistance();
-
-			//修改地图前判断是否吃到食物
-			if (csGameData.GetMap(stNewSnakeHead).enMapType == Game_Data::Map_Type::Food)
-			{
-				bEatFood = true;
-			}
-
-			//设置原先蛇头位置下的数据、类型
-			csGameData.GetMap(csGameData.GetSnakeHead()) = Game_Data::Map
-			{
-				enCurrentHeadDirect,
-				Game_Data::Map_Type::Body,//（如果只有两格的情况下，尾部会将此身体修改为尾部，如果吃到食物则此身体不用改变）
-			};
-
-			//设置新蛇头位置下的数据、类型
-			csGameData.GetMap(stNewSnakeHead) = Game_Data::Map
-			{
-				enCurrentHeadDirect,
-				Game_Data::Map_Type::Head,
-			};
-
-			//设置新蛇头位置
-			csGameData.GetSnakeHead() = stNewSnakeHead;
+			return false;//直接返回，剩下都不需要更新
 		}
-		
 
-		//吃到食物的处理
-		if (bEatFood)
+		//递增移动距离
+		csGameData.IncTravelDistance();
+		//修改地图前判断是否吃到食物
+		bool bEatFood = csGameData.GetMap(stNewSnakeHead).enMapType == Game_Data::Map_Type::Food;
+
+		//没有吃到食物才清除蛇尾
+		My_Point stNewSnakeTail;
+		if (!bEatFood)
+		{
+			//保存当前蛇尾方向
+			Game_Data::Move_Direct enCurrentTailDirect = csGameData.GetTailDirect();
+
+			//获取蛇尾向当前方向移动一格的位置
+			stNewSnakeTail = csGameData.GetSnakeTail() + Game_Data::stSnakeMove[(long)enCurrentTailDirect];
+			//越界环绕
+			csGameData.Surround(stNewSnakeTail);
+
+			//清除原先蛇尾位置下的数据、类型
+			csGameData.GetMap(csGameData.GetSnakeTail()) = Game_Data::Map
+			{
+				Game_Data::Move_Direct::No_Move,
+				Game_Data::Map_Type::Blank,
+			};
+		}
+
+		//清除原先蛇头位置并更新蛇头，这个操作在清除蛇尾之后，避免头尾相交改变蛇尾数据
+
+		//设置原先蛇头位置下的数据、类型
+		csGameData.GetMap(csGameData.GetSnakeHead()) = Game_Data::Map
+		{
+			enCurrentHeadDirect,
+			Game_Data::Map_Type::Body,//（如果只有两格的情况下，尾部会将此身体修改为尾部，如果吃到食物则此身体不用改变）
+		};
+
+		//设置新蛇头位置下的数据、类型
+		csGameData.GetMap(stNewSnakeHead) = Game_Data::Map
+		{
+			enCurrentHeadDirect,
+			Game_Data::Map_Type::Head,
+		};
+
+		//设置新蛇头位置
+		csGameData.GetSnakeHead() = stNewSnakeHead;
+
+		//没有吃到食物最后才设置新蛇尾
+		if (!bEatFood)
+		{
+			//设置新蛇尾位置下的类型，数据不变
+			csGameData.GetMap(stNewSnakeTail).enMapType = Game_Data::Map_Type::Tail;
+
+			//设置新蛇尾位置
+			csGameData.GetSnakeTail() = stNewSnakeTail;
+		}
+		else//吃到食物的处理
 		{
 			//递增蛇长度
 			csGameData.IncSnakeLength();
@@ -245,33 +263,6 @@ public:
 			{//如果不是吃完全部再生成那么直接生成，否则判断当前食物数是否为0（短路求值特性，左边为false才测试右侧值）
 				ProduceFood(csGameData);
 			}
-
-			//不移动蛇尾
-			return true;
-		}
-
-		//更新蛇尾
-		{
-			//保存当前蛇尾方向
-			Game_Data::Move_Direct enCurrentTailDirect = csGameData.GetTailDirect();
-
-			//将蛇尾向当前方向移动一格
-			My_Point stNewSnakeTail = csGameData.GetSnakeTail() + Game_Data::stSnakeMove[(long)enCurrentTailDirect];
-			//越界环绕
-			csGameData.Surround(stNewSnakeTail);
-
-			//清除原先蛇尾位置下的数据、类型
-			csGameData.GetMap(csGameData.GetSnakeTail()) = Game_Data::Map
-			{
-				Game_Data::Move_Direct::No_Move,
-				Game_Data::Map_Type::Blank,
-			};
-
-			//设置新蛇尾位置下的类型，数据不变
-			csGameData.GetMap(stNewSnakeTail).enMapType = Game_Data::Map_Type::Tail;
-
-			//设置新蛇尾位置
-			csGameData.GetSnakeTail() = stNewSnakeTail;
 		}
 		
 		return true;
